d70caff7b540a1536432e4f6de397c0da5ed3347
Revivedaniel <mlg.danielstark@gmail.com>
2021-07-06 03:00:29 -0700

    Moved some variables to global, re ordered eventlistener functions, cleared inteval when completed

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,17 +2,16 @@
 let functions = {
     //countdown method
     countdown: function(time) {
-        var timeLeft = time;
+        timeLeft = time;
       
-        var timeInterval = setInterval(function () {
-          if (timeLeft > 1) {
+        timeInterval = setInterval(function () {
+           if (timeLeft > 1) {
             timeStamp.textContent = String(timeLeft);
             timeLeft--;
-          }else {
+          } else {
             timeStamp.textContent = '0';
             clearInterval(timeInterval);
             alert("You ran out of time. Try again!");
-            quizFailed = true;
           }
         }, 1000);
       },
@@ -24,9 +23,12 @@ let functions = {
     showNextQuestion: function (questionNumber) {
         let questionIndex = (questionNumber - 1)
         questionIndex++
+        //After answering the last question correctly stop interval,  write time to score, and display allDone
         if (questionIndex >= 6) {
+            clearInterval(timeInterval)
+            allDoneScoreEL.textContent = String(timeLeft + 1);
             allDoneEL.classList.remove("hidden")
-        } else{
+        } else {
         questionsEL[questionIndex].classList.remove("hidden")
         }
     },
@@ -45,19 +47,18 @@ let functions = {
         }, 2000)
     },
 };
-//timer active
-var quizStarted = false;
-var quizDone = false;
-var quizFailed = false;
+var timeLeft = ""
+var timeInterval = ""
+//question index
 var currentQuestion = 0;
-//place all articles in variables
+//articles in variables
 var beginEL = document.querySelector("#begin");
 var questionsEL = document.querySelectorAll(".question");
 var allDoneEL = document.querySelector("#allDone");
+var allDoneScoreEL = allDoneEL.querySelector("span")
 var highScoresEL = document.querySelector("#highScores");
 var correct = document.querySelector("#correct")
 var incorrect = document.querySelector("#incorrect")
-//place all buttons in variables
 //Start quiz button
 var startQuizBttn = beginEL.querySelector("button");
 //array of answer buttons for questions
@@ -71,11 +72,10 @@ var question6buttons = questionsEL[5].querySelectorAll("button");
 var initials = allDoneEL.querySelector("#initials");
 var submit = allDoneEL.querySelector("#submit");
 var goBackBttns = highScoresEL.querySelectorAll("button")
-//place the ul for high scores in a variable
+//ul for high scores in a variable
 var scoresGrid = highScoresEL.querySelector("ul")
-//place time in variable
+//time in variable
 var timeStamp = document.querySelector("#timeStamp")
-//create timer
 //when start quiz is pressed start timer, hide first article, and show first question
 startQuizBttn.addEventListener("click", function () {
     functions.countdown(98);
@@ -85,19 +85,19 @@ startQuizBttn.addEventListener("click", function () {
 //when question 1 is correct hide current article, itterate questionNumber, and display next question
 question1buttons[0].addEventListener("click", functions.incorrectAnswer)
 question1buttons[1].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[0]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[0]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 question1buttons[2].addEventListener("click", functions.incorrectAnswer)
 question1buttons[3].addEventListener("click", functions.incorrectAnswer)
 //when question 2 is correct hide current article, itterate questionNumber, and display next question
 question2buttons[0].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[1]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[1]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 question2buttons[1].addEventListener("click", functions.incorrectAnswer)
 question2buttons[2].addEventListener("click", functions.incorrectAnswer)
@@ -106,10 +106,10 @@ question2buttons[3].addEventListener("click", functions.incorrectAnswer)
 question3buttons[0].addEventListener("click", functions.incorrectAnswer)
 question3buttons[1].addEventListener("click", functions.incorrectAnswer)
 question3buttons[2].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[2]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[2]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 question3buttons[3].addEventListener("click", functions.incorrectAnswer)
 //when question 4 is correct hide current article, itterate questionNumber, and display next question
@@ -117,17 +117,17 @@ question4buttons[0].addEventListener("click", functions.incorrectAnswer)
 question4buttons[1].addEventListener("click", functions.incorrectAnswer)
 question4buttons[2].addEventListener("click", functions.incorrectAnswer)
 question4buttons[3].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[3]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[3]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 //when question 5 is correct hide current article, itterate questionNumber, and display next question
 question5buttons[0].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[4]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[4]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 question5buttons[1].addEventListener("click", functions.incorrectAnswer)
 question5buttons[2].addEventListener("click", functions.incorrectAnswer)
@@ -135,12 +135,11 @@ question5buttons[3].addEventListener("click", functions.incorrectAnswer)
 //when question 6 is correct hide current article, itterate questionNumber, and display next question
 question6buttons[0].addEventListener("click", functions.incorrectAnswer)
 question6buttons[1].addEventListener("click", function() {
-    functions.correctAnswer();
-    functions.hideCurrentArticle(questionsEL[5]);
     currentQuestion++
+    functions.hideCurrentArticle(questionsEL[5]);
     functions.showNextQuestion(currentQuestion);
+    functions.correctAnswer();
 })
 question6buttons[2].addEventListener("click", functions.incorrectAnswer)
 question6buttons[3].addEventListener("click", functions.incorrectAnswer)
-//when timer runs out hide current article, write time to score, and display allDone
 //when last question is answered stop timer, hide last question, write time to score, and display allDone
