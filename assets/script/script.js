09ca527a4d6ef7fcc44ede1529747a235c6484b0
Revivedaniel <mlg.danielstark@gmail.com>
2021-07-06 02:05:38 -0700

    Moved functions object to top, created correct and incorrect variables, and made the countdown function pass through time

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,51 +1,8 @@
-//timer active
-var quizStarted = false;
-var quizDone = false;
-var quizFailed = false;
-var currentQuestion = 0;
-//place all articles in variables
-var beginEL = document.querySelector("#begin");
-var questionsEL = document.querySelectorAll(".question");
-var allDoneEL = document.querySelector("#allDone");
-var highScoresEL = document.querySelector("#highScores");
-//place all buttons in variables
-//Start quiz button
-var startQuizBttn = beginEL.querySelector("button");
-//array of answer buttons for questions
-var question1buttons = questionsEL[0].querySelectorAll("button");
-var question2buttons = questionsEL[1].querySelectorAll("button");
-var question3buttons = questionsEL[2].querySelectorAll("button");
-var question4buttons = questionsEL[3].querySelectorAll("button");
-var question5buttons = questionsEL[4].querySelectorAll("button");
-var question6buttons = questionsEL[5].querySelectorAll("button");
-//allDone buttons
-var initials = allDoneEL.querySelector("#initials");
-var submit = allDoneEL.querySelector("#submit");
-var goBackBttns = highScoresEL.querySelectorAll("button")
-//place the ul for high scores in a variable
-var scoresGrid = highScoresEL.querySelector("ul")
-//place time in variable
-var timeStamp = document.querySelector("#timeStamp")
-//create timer
-//when start quiz is pressed start timer, hide first article, and show first question
-startQuizBttn.addEventListener("click", function () {
-    functions.countdown();
-    functions.hideCurrentArticle(beginEL);
-    functions.showNextQuestion(currentQuestion);
-});
-//when question 1 is correct hide current article, itterate questionNumber, and display next question
-//when question 2 is correct hide current article, itterate questionNumber, and display next question
-//when question 3 is correct hide current article, itterate questionNumber, and display next question
-//when question 4 is correct hide current article, itterate questionNumber, and display next question
-//when question 5 is correct hide current article, itterate questionNumber, and display next question
-//when question 6 is correct hide current article, itterate questionNumber, and display next question
-//when timer runs out hide current article, write time to score, and display allDone
-//when last question is answered stop timer, hide last question, write time to score, and display allDone
 //all methods object
 let functions = {
     //countdown method
-    countdown: function() {
-        var timeLeft = 98;
+    countdown: function(time) {
+        var timeLeft = time;
       
         var timeInterval = setInterval(function () {
           if (timeLeft > 1) {
@@ -72,5 +29,65 @@ let functions = {
         } else{
         questionsEL[questionIndex].classList.remove("hidden")
         }
-    }
+    },
+    //Question is correct
+    correctAnswer: function () {
+        correct.classList.remove("hidden")
+        setTimeout(function () {
+            correct.classList.add("hidden")
+        }, 2000)
+    },
+    //Question is incorrect
+    incorrectAnswer: function () {
+        incorrect.classList.remove("hidden")
+        setTimeout(function () {
+            incorrect.classList.add("hidden")
+        }, 2000)
+    },
 };
+//timer active
+var quizStarted = false;
+var quizDone = false;
+var quizFailed = false;
+var currentQuestion = 0;
+//place all articles in variables
+var beginEL = document.querySelector("#begin");
+var questionsEL = document.querySelectorAll(".question");
+var allDoneEL = document.querySelector("#allDone");
+var highScoresEL = document.querySelector("#highScores");
+var correct = document.querySelector("#correct")
+var incorrect = document.querySelector("#incorrect")
+//place all buttons in variables
+//Start quiz button
+var startQuizBttn = beginEL.querySelector("button");
+//array of answer buttons for questions
+var question1buttons = questionsEL[0].querySelectorAll("button");
+var question2buttons = questionsEL[1].querySelectorAll("button");
+var question3buttons = questionsEL[2].querySelectorAll("button");
+var question4buttons = questionsEL[3].querySelectorAll("button");
+var question5buttons = questionsEL[4].querySelectorAll("button");
+var question6buttons = questionsEL[5].querySelectorAll("button");
+//allDone buttons
+var initials = allDoneEL.querySelector("#initials");
+var submit = allDoneEL.querySelector("#submit");
+var goBackBttns = highScoresEL.querySelectorAll("button")
+//place the ul for high scores in a variable
+var scoresGrid = highScoresEL.querySelector("ul")
+//place time in variable
+var timeStamp = document.querySelector("#timeStamp")
+//create timer
+//when start quiz is pressed start timer, hide first article, and show first question
+startQuizBttn.addEventListener("click", function () {
+    functions.countdown(98);
+    functions.hideCurrentArticle(beginEL);
+    functions.showNextQuestion(currentQuestion);
+});
+//when question 1 is correct hide current article, itterate questionNumber, and display next question
+question1buttons[0].addEventListener("click", )
+//when question 2 is correct hide current article, itterate questionNumber, and display next question
+//when question 3 is correct hide current article, itterate questionNumber, and display next question
+//when question 4 is correct hide current article, itterate questionNumber, and display next question
+//when question 5 is correct hide current article, itterate questionNumber, and display next question
+//when question 6 is correct hide current article, itterate questionNumber, and display next question
+//when timer runs out hide current article, write time to score, and display allDone
+//when last question is answered stop timer, hide last question, write time to score, and display allDone
