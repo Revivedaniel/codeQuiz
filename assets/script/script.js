d6ef08619e5b04df48a0631cea63afa10ec89255
Revivedaniel <mlg.danielstark@gmail.com>
2021-07-06 01:40:58 -0700

    Added currentQuestion variable, created functions object and continued comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@
 var quizStarted = false;
 var quizDone = false;
 var quizFailed = false;
+var currentQuestion = 0;
 //place all articles in variables
 var beginEL = document.querySelector("#begin");
 var questionsEL = document.querySelectorAll(".question");
@@ -26,23 +27,50 @@ var scoresGrid = highScoresEL.querySelector("ul")
 //place time in variable
 var timeStamp = document.querySelector("#timeStamp")
 //create timer
-// Timer that counts down from 99
-function countdown() {
-    var timeLeft = 99;
-  
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timeStamp.textContent = String(timeLeft);
-        timeLeft--;
-      }else {
-        timeStamp.textContent = '0';
-        clearInterval(timeInterval);
-        alert("You ran out of time. Try again!");
-        quizFailed = true;
-      }
-    }, 1000);
-  }
-//write timer to time element
 //when start quiz is pressed start timer, hide first article, and show first question
+startQuizBttn.addEventListener("click", function () {
+    functions.countdown();
+    functions.hideCurrentArticle(beginEL);
+    functions.showNextQuestion(currentQuestion);
+});
+//when question 1 is correct hide current article, itterate questionNumber, and display next question
+//when question 2 is correct hide current article, itterate questionNumber, and display next question
+//when question 3 is correct hide current article, itterate questionNumber, and display next question
+//when question 4 is correct hide current article, itterate questionNumber, and display next question
+//when question 5 is correct hide current article, itterate questionNumber, and display next question
+//when question 6 is correct hide current article, itterate questionNumber, and display next question
 //when timer runs out hide current article, write time to score, and display allDone
 //when last question is answered stop timer, hide last question, write time to score, and display allDone
+//all methods object
+let functions = {
+    //countdown method
+    countdown: function() {
+        var timeLeft = 98;
+      
+        var timeInterval = setInterval(function () {
+          if (timeLeft > 1) {
+            timeStamp.textContent = String(timeLeft);
+            timeLeft--;
+          }else {
+            timeStamp.textContent = '0';
+            clearInterval(timeInterval);
+            alert("You ran out of time. Try again!");
+            quizFailed = true;
+          }
+        }, 1000);
+      },
+    //hide current article method
+    hideCurrentArticle: function(element)  {
+        element.classList.add("hidden")
+    },
+    //show next question method
+    showNextQuestion: function (questionNumber) {
+        let questionIndex = (questionNumber - 1)
+        questionIndex++
+        if (questionIndex >= 6) {
+            allDoneEL.classList.remove("hidden")
+        } else{
+        questionsEL[questionIndex].classList.remove("hidden")
+        }
+    }
+};
