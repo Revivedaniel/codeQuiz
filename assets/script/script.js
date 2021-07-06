63c422d76d79fb5d2ee9e204566413e49a917b66
Revivedaniel <mlg.danielstark@gmail.com>
2021-07-06 00:08:30 -0700

    Started writing out js steps

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+//place all articles in variables
+//place all buttons in variables
+//place the ul for high scores in a variable
+//place time in variable
+//create timer
+//write timer to time element
+//when start quiz is pressed start timer, hide first article, and show first question
+//when timer runs out hide current article, write time to score, and display allDone
+//when last question is answered stop timer, hide last question, write time to score, and display allDone
